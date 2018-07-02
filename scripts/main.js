703928c6f703b40e7539bdb57017dfe971229237
Dem <d3m1ank@gmail.com>
2018-07-02 16:12:38 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+var pikaSwap = document.getElementById("pikachu");
+// pikaSwap.setAttribute("src", "images/PikachuStrong.png");
+
+var pikaFriendly = document.getElementById("Friendly");
+pikaFriendly.onmouseover = function() {
+  pikaSwap.setAttribute("src", "images/PikachuFriendly.png");
+};
+pikaFriendly.onmouseout = function() {
+  pikaSwap.setAttribute("src", "images/Pikachu.png");
+};
+
+var pikaLoyal = document.getElementById("Loyal");
+pikaLoyal.onmouseover = function() {
+  pikaSwap.setAttribute("src", "images/PikachuLoyal.png");
+};
+pikaLoyal.onmouseout = function() {
+  pikaSwap.setAttribute("src", "images/Pikachu.png");
+};
+
+var pikaKind = document.getElementById("Kind");
+pikaKind.onmouseover = function() {
+  pikaSwap.setAttribute("src", "images/PikachuKind.png");
+};
+pikaKind.onmouseout = function() {
+  pikaSwap.setAttribute("src", "images/Pikachu.png");
+};
+
+var pikaBrave = document.getElementById("Brave");
+pikaBrave.onmouseover = function() {
+  pikaSwap.setAttribute("src", "images/PikachuBrave.png");
+};
+pikaBrave.onmouseout = function() {
+  pikaSwap.setAttribute("src", "images/Pikachu.png");
+};
+
+var pikaStrong = document.getElementById("Strong");
+pikaStrong.onmouseover = function() {
+  pikaSwap.setAttribute("src", "images/PikachuStrong.png");
+};
+pikaStrong.onmouseout = function() {
+  pikaSwap.setAttribute("src", "images/Pikachu.png");
+};
